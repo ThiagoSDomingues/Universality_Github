22869107d0c011a5f487abf57d32465d7524e875
Thiago Siqueira Domingues <68281601+ThiagoSDomingues@users.noreply.github.com>
2026-01-07 18:32:41 -0300

    Update plots.py

--- scripts/plots.py
+++ scripts/plots.py
@@ -27,9 +27,20 @@ MM_TO_PT = 72 / 25.4  # ≈ 2.835
 MIN_FONT_PT = 2.0 * MM_TO_PT  # ≈ 5.7 pt
 SUB_FONT_PT = 1.5 * MM_TO_PT  # ≈ 4.3 pt
 
-
+# Apply global adjustments
+plt.rcParams.update({
+    "font.size": MIN_FONT_PT,
+    "axes.labelsize": MIN_FONT_PT,
+    "axes.titlesize": MIN_FONT_PT,
+    "xtick.labelsize": SUB_FONT_PT,
+    "ytick.labelsize": SUB_FONT_PT,
+    "legend.fontsize": SUB_FONT_PT,
+})
 
 # Python sanity check to ensure that all figures are in the PRC requirements!
 for text in fig.findobj(match=plt.Text):
     if text.get_fontsize() < MIN_FONT_PT:
         print("Warning:", text.get_text())
+
+# Save any figure as: 
+#plt.savefig("figure1.pdf", bbox_inches="tight")
